4ccdc01d895ffdcf7061246096f0fad612e2a11d
Александр Садовский <sadovskiyaa@list.ru>
2026-03-18 17:56:32 +0300

    Knowing-Django-template  v.1

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,11 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-]
-
-
-urlpatterns = [
-    path("", views.home, name="home"),  # главная страница каталога
+    path("", views.home, name="home"),  # главная страница (первый вариант)
     path("contacts/", views.contacts, name="contacts"),  # страница контактов
+    path(
+        "product/<int:pk>/", views.product_detail, name="product_detail"
+    ),  # детальная страница товара
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,19 +1,30 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
-
-
-def index(request):
-    return HttpResponse("Добро пожаловать в каталог!")
-
-
-# from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product
 
 
 def home(request):
-    return render(request, "catalog/home.html")
+    """
+    Главная страница каталога со списком товаров
+    """
+
+    products = Product.objects.all().order_by("-created_at")
+
+    context = {"products": products}
+    return render(request, "catalog/home.html", context)
 
 
 def contacts(request):
     return render(request, "catalog/contacts.html")
+
+
+def product_detail(request, pk):
+    """
+    Отображает страницу с подробной информацией о товаре
+    """
+    # Ищем товар по id в базе данных
+    # Если товар не найден — автоматически вернется 404 ошибка
+    product = get_object_or_404(Product, id=pk)
+
+    # Передаем найденный товар в шаблон
+    context = {"product": product}
+    return render(request, "catalog/product_detail.html", context)
