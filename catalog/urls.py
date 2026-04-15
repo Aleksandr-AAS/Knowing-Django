77070f49cbff0ce87414c8580fd5419eb3b81486
Александр Садовский <sadovskiyaa@list.ru>
2026-04-15 17:04:53 +0300

    Knowing-Django-Forms v.1

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = "catalog"
 urlpatterns = [
     # Главная страница (CBV)
     path("", views.HomeView.as_view(), name="home"),
@@ -8,4 +9,16 @@ urlpatterns = [
     path("contacts/", views.ContactsView.as_view(), name="contacts"),
     # Детальная страница товара (CBV)
     path("product/<int:pk>/", views.ProductDetailView.as_view(), name="product_detail"),
+    path("products/", views.ProductListView.as_view(), name="product_list"),
+    path("products/create/", views.ProductCreateView.as_view(), name="product_create"),
+    path(
+        "products/<int:pk>/update/",
+        views.ProductUpdateView.as_view(),
+        name="product_update",
+    ),
+    path(
+        "products/<int:pk>/delete/",
+        views.ProductDeleteView.as_view(),
+        name="product_delete",
+    ),
 ]
